3f1e817eeb0653b7df682629db4c5736527bb598
Max Leonard <maxhleonard@gmail.com>
2021-03-17 14:21:00 -0700

    new position update script

--- input.py
+++ input.py
@@ -1,6 +1,6 @@
 #Edit these daily to setup the bracket orders
-LONG_TICKERS = ["AAPL"]
-SHORT_TICKERS = ["SNAP"]
+LONG_TICKERS = []
+SHORT_TICKERS = []
 
 #How much $ you want to risk on each trade
 TRADE_RISK = 100
@@ -10,3 +10,11 @@ RISK_TYPE = "dollar" #dollar/percent
 
 
 ACCOUNT_STRING = "DU2614887"
+
+ATR_UP = 2
+ATR_DOWN = 1.5
+
+STOP_LIMIT_SPREAD = 0.1
+
+
+IGNORE = []
--- positionUpdate.py
+++ positionUpdate.py
@@ -9,14 +9,8 @@ from ibapi.order_state import OrderState
 import time
 import threading
 import datetime
-from input import LONG_TICKERS, SHORT_TICKERS, TRADE_RISK, ACCOUNT_PERCENT, RISK_TYPE, ACCOUNT_STRING, PROFIT_LEVEL
+from input import LONG_TICKERS, SHORT_TICKERS, TRADE_RISK, ACCOUNT_PERCENT, RISK_TYPE, ACCOUNT_STRING, IGNORE
 import sys
-import json
-
-
-print("Retrieving Local Stage Data...")
-with open("data.json") as f:
-	data = json.load(f)
 
 
 
@@ -24,227 +18,144 @@ class IBapi(EWrapper, EClient):
 	def __init__(self):
 		EClient.__init__(self, self)
 
-		self.nextOrderId = -1
 		self.positions = {}
-		self.pnl_req_ids = {}
-		self.pnl_recieved = set()
-		self.pnl_expected = -1
-		self.price_req_ids = {}
-		self.price_recieved = set()
-		self.price_expected = -1
+		self.currentDataReqId = 0
+		self.dataReqIds = {}
 
 
-
-	def check_stages(self):
-		for symbol, info in self.positions.items():
-			if symbol in data["stage0"]["short"]: 
-				if info["size"] != 0 and len(info["orders"]) == 2:
-					data["stage0"]["short"].remove(symbol)
-					data["stage1"]["short"].append(symbol)
-					print(symbol, "moved from Stage 0 to Stage 1 (short)")
-				elif info["size"] == 0 and len(info["orders"]) ==0:
-					data["stage0"]["short"].remove(symbol)
-					print(symbol, "position closed during day (bracket order).")
-			elif symbol in data["stage0"]["long"]:
-				if info["size"] != 0 and len(info["orders"]) == 2:
-					data["stage0"]["long"].remove(symbol)
-					data["stage1"]["long"].append(symbol)
-					print(symbol, "moved from Stage 0 to Stage 1 (long)")
-				elif info["size"] == 0 and len(info["orders"]) ==0:
-					data["stage0"]["long"].remove(symbol)
-					print(symbol, "position closed during day (bracket order).")
-
-			elif symbol in data["stage1"]["short"]:
-				if info["size"] == 0 and len(info["orders"]) == 0:
-					data["stage1"]["short"].remove(symbol)
-					print(symbol, "position closed during day (bracket order).")
-			elif symbol in data["stage1"]["long"]:
-				if info["size"] == 0 and len(info["orders"]) == 0:
-					data["stage1"]["long"].remove(symbol)
-					print(symbol, "position closed during day (bracket order).")
-
-			elif symbol in data["stage2"]["short"]:
-				if info["size"] == 0 and len(info["orders"]) == 0:
-					data["stage2"]["short"].remove(symbol)
-					print(symbol, "position closed during day (stop order).")
-			elif symbol in data["stage2"]["long"]:
-				if info["size"] == 0 and len(info["orders"]) == 0:
-					data["stage2"]["long"].remove(symbol)
-					print(symbol, "position closed during day (stop order).")
-
-		
-		print("Retrieving position profits...")
-		self.advance_stage1()
-			
-
-
-	def advance_stage1(self):
-
-		self.pnl_expected = len(data["stage1"]["long"]) + len(data["stage1"]["short"])
-		if self.pnl_expected == 0:
-			print("No Stage 1 positions, updating Stage 2 stops...")
-			self.advance_stage2()
-		reqId = 0
-		for symbol in data["stage1"]["long"]:
-			conId = self.positions[symbol]["contract"].conId
-			self.pnl_req_ids[reqId] = symbol
-			self.reqPnlSingle(reqId, ACCOUNT_STRING, "", conId)
-			reqId += 1
-
-		for symbol in data["stage1"]["short"]:
-			conId = self.positions[symbol]["contract"].conId
-			self.pnl_req_ids[reqId] = symbol
-			self.reqPnlSingle(reqId, ACCOUNT_STRING, "", conId)
-			reqId += 1
-
-	def advance_stage2(self):
-
-		self.price_expected = len(data["stage2"]) + len(data["stage2"])
-		if self.price_expected == 0:
-			print("No Stage 2 positions...")
-			self.disconnect()
-		reqId = 0
-		for symbol in data["stage2"]["long"]:
-			info = self.positions[symbol]
-			for orderId in info["orders"]:
-				self.cancelOrder(orderId)
-			self.reqHistoricalData(reqId, info["contract"], "", "1 D", "1 day", 1, 1, False, [])
-			self.price_req_ids[reqId] = symbol
-			reqId += 1
-
-		for symbol in data["stage2"]["short"]:
-			info = self.positions[symbol]
-			for orderId in info["orders"]:
-				self.cancelOrder(orderId)
-			self.reqHistoricalData(reqId, info["contract"], "", "1 D", "1 day", 1, 1, False, [])
-			self.price_req_ids[reqId] = symbol
-			reqId += 1
-
-	def update_stops(self):
-
-		for symbol in data["stage2"]["long"]:
-			info = self.positions[symbol]
-			order = Order()
-			order.action = "SELL"
-			order.orderType = "STP"
-			order.auxPrice = info["bar"].low
-			#self.placeOrder(self.nextOrderId, info["contract"], order)
-			self.nextOrderId += 1
-
-		for symbol in data["stage2"]["short"]:
-			info = self.positions[symbol]
-			order = Order()
-			order.action = "BUY"
-			order.orderType = "STP"
-			order.auxPrice = info["bar"].high
-			#self.placeOrder(self.nextOrderId, info["contract"], order)
-			self.nextOrderId += 1
-
-		self.disconnect()
-
-	def error(self, reqId, errorCode, errorString):
-
-		print("Error: ", reqId, " ", errorCode, " ", errorString)
-
-	def nextValidId(self, orderId: int):
-
-		super().nextValidId(orderId)
-		self.nextOrderId = orderId
-
-	def historicalData(self, reqId:int, bar):
-		
-		self.price_recieved.add(reqId)
-		symbol = self.price_req_ids[reqId]
-		self.positions[symbol]["bar"] = bar
-		if len(self.price_recieved) == self.price_expected:
-			self.update_stops()
-		
-		
 	def position(self, account: str, contract: Contract, position: float, avgCost: float):
-		self.positions[contract.symbol] = {"contract":contract, "size":position, "orders":[], "avgCost":avgCost}
+
+		if position != 0 and contract.symbol not in IGNORE:
+			self.positions[contract.symbol] = {"position":position}
 
 	def positionEnd(self):
-		print("Retrieving IB order data...")
+
+		print("Current Positions Recieved...")
 		self.reqAllOpenOrders()
 
-	def pnlSingle(self, reqId: int, pos: int, dailyPnL: float, unrealizedPnL: float, realizedPnL: float, value: float):
-
-		print(reqId)
-		self.pnl_recieved.add(reqId)
-		symbol = self.pnl_req_ids[reqId]
-		info = self.positions[symbol]
-		basis = info["size"] * info["avgCost"]
-		perChange = (value - basis) / basis
-		if perChange >= PROFIT_LEVEL:
-			if symbol in data["stage1"]["short"]:
-				data["stage1"]["short"].remove(symbol)
-				data["stage2"]["short"].append(symbol)
-				print(symbol, "moved from Stage 1 to Stage 2 (short)")
-			elif symbol in data["stage1"]["long"]:
-				data["stage1"]["long"].remove(symbol)
-				data["stage2"]["long"].append(symbol)
-				print(symbol, "moved from Stage 1 to Stage 2 (long)")
-
-		if len(self.pnl_recieved) == self.pnl_expected:
-			print("Updating Stage 2 stops...")
-			self.advance_stage2()
-
 	def openOrder(self, orderId: int, contract: Contract, order: Order, orderState: OrderState):
-		#super().openOrder(orderId, contract, order, orderState)
-		#print("OpenOrder. PermId: ", order.permId, "ClientId:", order.clientId, " OrderId:", orderId, "Account:", order.account, "Symbol:", contract.symbol, "SecType:", contract.secType, "Exchange:", contract.exchange, "Action:", order.action, "OrderType:", order.orderType, "TotalQty:", order.totalQuantity, "CashQty:", order.cashQty, "LmtPrice:", order.lmtPrice, "AuxPrice:", order.auxPrice, "Status:", orderState.status)
-		if contract.symbol in self.positions.keys():
-			self.positions[contract.symbol]["orders"] += orderId
 
-	def orderStatus(self, orderId: int, status: str, filled: float, remaining: float, avgFillPrice: float, permId: int, parentId: int, lastFillPrice: float, clientId: int, whyHeld: str, mktCapPrice: float):
-		#super().orderStatus(orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice)
-		#print("OrderStatus. Id:", orderId, "Status:", status, "Filled:", filled, "Remaining:", remaining, "AvgFillPrice:", avgFillPrice, "PermId:", permId, "ParentId:", parentId, "LastFillPrice:", lastFillPrice, "ClientId:", clientId, "WhyHeld:", whyHeld, "MktCapPrice:", mktCapPrice)
-		pass
+		this_symbol = contract.symbol
+		if order.orderType == "STP" and this_symbol in self.positions.keys():
+		
+			self.positions[this_symbol]["contract"] = contract
+			self.positions[this_symbol]["order"] = order
+			self.positions[this_symbol]["orderId"] = orderId
 
 	def openOrderEnd(self):
-		self.check_stages()
+
+		to_remove = []
+		for symbol, info in self.positions.items():
+			try:
+				test = info["order"]
+			except:
+				to_remove.append(symbol)
+
+		for symbol in to_remove:
+			self.positions.pop(symbol)
+
+		print("Open Orders Recieved, Checking Price Data...")
+		for symbol, info in self.positions.items():
+
+			self.reqHistoricalData(self.currentDataReqId, info["contract"], "", "1 D", "1 day", "TRADES", 1, 1, False, [])
+			self.dataReqIds[self.currentDataReqId] = symbol
+			self.currentDataReqId += 1
+
+	def historicalData(self, reqId, bar):
+
+		this_symbol = self.dataReqIds[reqId]
+		self.positions[this_symbol]["bar"] = bar
+
+		if self.completeData():
+			print("Price Data Recieved...")
+			self.beginUpdate()
+
+	def completeData(self):
+
+		for symbol, info in self.positions.items():
+			try:
+				test = info["bar"]
+			except:
+				return False
+
+		return True
 
 
+	def beginUpdate(self):
 
-	
+		print("The program will now loop through your positions with stops and suggest new prices...")
+		print("-To take no action/skip a certain position, just hit enter or type N and then hit enter...")
+		print("-To adjust the stop to the suggested price type Y and hit enter...")
+		print("-To adjust the stop to a custom price just type in that price and hit enter...")
+
+		for symbol, info in self.positions.items():
+
+			if info["position"] < 0:
+				suggested = info["bar"].high
+			elif info["position"] > 0:
+				suggested = info["bar"].low
+
+			print("\n\n{sybmol} - Position: {pos}".format(sybmol=symbol, pos=info["position"]))
+			print("Current Stop: {current}, Suggested New Stop: {suggested}".format(current=info["order"].auxPrice, suggested=suggested))
+			while True:
+				res = input("Please type (Enter/Y/N/customprice): ")
+				try:
+					res_num = float(res.strip)
+				except:
+					res_num = None
+
+				if res in ["", "N", "n"] or res.isspace():
+					break
+
+				elif res in ["Y", "y"]:
+					new_order = info["order"]
+					new_order.auxPrice = suggested
+					self.placeOrder(info["orderId"], info["contract"], new_order)
+					break
+
+				elif res_num != None:
+					new_order = info["order"]
+					new_order.auxPrice = res_num
+					self.placeOrder(info["orderId"], info["contract"], new_order)
+					break
+
+				else:
+					print("Incorrectly formatted response...")
+
+		print("Program finished, shutting down...")
+		print("Any following errors are to be ignored...")
+		time.sleep(5)
+		self.disconnect()
+
+now = datetime.datetime.today()
+if (now.hour > 9 and now.hour < 17) or (now.hour == 9 and now.minute >= 30):
+	MARKET_OPEN = True
+else:
+	MARKET_OPEN = False
+
 
 def run_loop():
 	app.run()
 
+print("Configuring Connection...")
 
 app = IBapi()
-app.connect('127.0.0.1', 7496, 0)
-print("Connecting to IBAPI...")
-time.sleep(8)
+app.connect('127.0.0.1', 7497, 104)
+
+time.sleep(5)
+
 thread = threading.Thread(target=run_loop)
 thread.start()
 
-now = datetime.datetime.today()
-'''
-last_time = datetime.datetime.strptime(data["last_update_positions"], "%m/%d/%Y, %H:%M:%S")
-if last_time.day == now.day and last_time.month == now.month:
-	print("Positions already updated today")
-	print("Exiting program...")
-	app.disconnect()
-	time.sleep(2)
-	sys.exit(0)
-'''
+time.sleep(5)
 
-print("Retrieving IB position data...")
 app.reqPositions()
 
-#thread.join()
-time.sleep(20)
-app.disconnect()
-sys.exit(0)
 
-print("Saving Local Stage Data...")
-data["last_update_positions"] = datetime.datetime.today().strftime("%m/%d/%Y, %H:%M:%S")
 
-with open("data.json", "w") as f:
-	json.dump(data, f)
-print(data)
-print("Exiting program...")
-time.sleep(2)
+
+
+
 
 
 
